4a95d6b0caf583c1f9bf880354ee67abce671d78
merieme soukrat <meriemesoukrat@gmail.com>
2016-06-03 14:09:51 +0100

    commit merieme

--- src/java/bean/Quartier.java
+++ src/java/bean/Quartier.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bean;
 
 import java.io.Serializable;
@@ -14,10 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
-/**
- *
- * @author MarouaneKH
- */
+
 @Entity
 public class Quartier implements Serializable {
 
--- src/java/service/UserFacade.java
+++ src/java/service/UserFacade.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package service;
 
 import bean.User;
@@ -10,10 +6,6 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-/**
- *
- * @author pc
- */
 @Stateless
 public class UserFacade extends AbstractFacade<User> {
 
